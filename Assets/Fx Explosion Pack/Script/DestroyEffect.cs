55d005c8fb4cb7e57683560551cb1ff95c501a54
Andrei Copaceanu <andrei.copaceanu@yahoo.com>
2022-02-06 11:22:42 +0000

    Commit 1

---
+++ Assets/Fx Explosion Pack/Script/DestroyEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour {
+
+    float time;
+
+	void Update ()
+	{
+        time += Time.deltaTime;
+		if(time>5.0f)
+        {
+            Destroy(gameObject);
+        }
+	
+	}
+}
